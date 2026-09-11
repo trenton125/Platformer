ac04b9d7580da699671a760dc330611a3e3d06e8
Trenton Haynes <trentonhaynes125@gmail.com>
2026-09-11 15:46:12 -0400

    Added sfml and green dot code.

--- Platformer/Platformer/main.cpp
+++ Platformer/Platformer/main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main() {
-	cout << "Sup World" << endl;
-	return 0;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
